23f17acf1f8e0736f83691f9004dc972ae14e84d
Roshan rai <94675864+Robot12r@users.noreply.github.com>
2024-10-26 15:06:56 +0000

    helloworld

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 int main(void){
-    printf("installed and compiled first program");
+    int m, n, sum;
+    printf("installed and compiled first program\n");
     printf("input two numbers");
-    scanf("%d,%d");
+    scanf("%d,%d",&m,&n);
+    sum = m + n;
+    printf("\n output %d\n", sum);
+    printf("nn\n\nnn\n");
+    printf("nn/n/nnn/n");
     return 0;
 }
+/*nn
+
+nn
+nn/n/n/nnn/n
+*/
