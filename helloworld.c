2aad4d32a1e7eec7552f7472cd5450c9c6573988
Roshan rai <94675864+Robot12r@users.noreply.github.com>
2024-10-26 15:12:20 +0000

    ok commit

--- helloworld.c
+++ helloworld.c
@@ -3,15 +3,16 @@
 int main(void){
     int m, n, sum;
     printf("installed and compiled first program\n");
-    printf("input two numbers");
+    printf("input two numbers\n");
     scanf("%d,%d",&m,&n);
     sum = m + n;
-    printf("\n output %d\n", sum);
+    printf("\n output %d\n", m+n);
     printf("nn\n\nnn\n");
     printf("nn/n/nnn/n");
     return 0;
 }
-/*nn
+/*
+nn
 
 nn
 nn/n/n/nnn/n
