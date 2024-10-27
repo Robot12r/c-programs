aad504a16b89bab494495d46fcf53424f2b1cb3a
Roshan rai <94675864+Robot12r@users.noreply.github.com>
2024-10-27 02:56:01 +0000

    26 s#	modified:   new1-26.c

--- new1-26.c
+++ new1-26.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 int main (void){
     int a = 5;
     int m = 2;
-    printf("%d,a-m");
-    return 0
+    float z = 2.5;
+    printf("%d",a-m);
+    printf("\nprint %d", a+m);
+    printf("\nprint %d", a*m);
+    printf("\nprint %d\n", a/m);
+    //printf("%f",pow(z,2));
+    return 0;
 }
