0d984b89922e6c4f47ce732890770435479768d9
Roshan rai <94675864+Robot12r@users.noreply.github.com>
2024-10-29 09:43:13 +0000

    modified some files

---
+++ area-of-ellipse.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    float a,b;
+    float pi = 3.1415;
+    printf("input a and b sep by , ");
+    scanf("%f, %f",&a,&b);
+    printf("Area of ellipse is %f",(pi*a*b));
+    return 0;
+
+}
